946d7062d44921bb471fc5e7dfbb79dea70d777d
Lucas Alves <64313623+Lucas7Alves@users.noreply.github.com>
2025-03-02 12:25:20 -0300

    put product

--- dsecommerce/src/main/java/com/devsuperior/dsecommerce/controllers/ProductController.java
+++ dsecommerce/src/main/java/com/devsuperior/dsecommerce/controllers/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,11 +40,20 @@ public class ProductController {
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
 		dto = productService.insert(dto);
+		
+		//Este URI permite retornar o codigo 201 no HTTP + nova URL do produto
 		URI uri = ServletUriComponentsBuilder
 				.fromCurrentRequest()
 				.path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
+		
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = productService.update(id, dto);
+		return ResponseEntity.ok(dto);
 	}
 
 }
--- dsecommerce/src/main/java/com/devsuperior/dsecommerce/services/ProductService.java
+++ dsecommerce/src/main/java/com/devsuperior/dsecommerce/services/ProductService.java
@@ -33,13 +33,21 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
+		return new ProductDTO(productRepository.save(entity));
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		Product entity = productRepository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);
+		return new ProductDTO(productRepository.save(entity));
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
 		entity.setDescription(dto.getDescription());
 		entity.setPrice(dto.getPrice());
 		entity.setImgUrl(dto.getImgUrl());
-		
-		entity = productRepository.save(entity);
-		
-		return new ProductDTO(entity);
 	}
 }
